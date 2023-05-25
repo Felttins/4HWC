3d075a2e299acc1df6040415cc8fa5346b1aa468
Lebedev Vitaly <11132166@mail.ru>
2023-05-25 17:33:55 +0300

    2

---
+++ Task 2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 - 11
+// 82 -> 10
+// 9012 -> 12
+
+int Number = Prompt("Введите число - ");
+int Summ = 0;
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    int Number = int.Parse(Console.ReadLine()!);
+    return Number;
+}
+
+Math(Number, Summ);
+
+void Math(int Number, int Summ)
+{
+        while (Number%10>0 || Number/10>0)
+        {
+            Summ = Summ + Number%10;
+            Number = Number/10;
+        }
+        Console.WriteLine($"Сумма цифр равна {Summ} ");
+}
